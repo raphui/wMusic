02ab08ecd1e04f13beff8365e86610172aa28c32
raphui <raphio134@hotmail.fr>
2013-03-20 03:11:51 -0700

    makefile for x86 arch working and web api w0.1

--- WebApi/routes/commander.js
+++ WebApi/routes/commander.js
@@ -1,7 +1,23 @@
+var net = require("net");
+var host = '127.0.0.1'
+var port = '1338';
+
+var socket = new net.Socket();
+socket.connect( port , host , function() {
+	console.log("Connected to: " + host + ":" + port );
+});
+
+exports.load = function( req , res ) {
+	res.send({action:'load' , status:'OK'});
+	socket.write("STREAMER#LOAD#" + req.params.url + "%" + req.params.name );
+};
+
 exports.play = function( req , res ) {
-	res.send([{action:'play'}]);
+	res.send({action:'play' , status:'OK'});
+	socket.write("STREAMER#PLAY#" + req.params.name );
 };
 
 exports.pause = function( req , res ) {
-	res.send([{action:'pause'}]);
+	res.send({action:'pause' , status:'OK'});
+	socket.write("STREAMER#PAUSE#" + req.params.name );
 };
--- WebApi/server.js
+++ WebApi/server.js
@@ -2,9 +2,10 @@ var express = require('express'),
 commander = require('./routes/commander');
  
 var app = express();
- 
-app.get('/play', commander.play);
-app.get('/pause', commander.pause);
+
+app.get('/load/:url/:name' , commander.load ); 
+app.get('/play/:name' , commander.play );
+app.get('/pause/:name' , commander.pause );
  
 app.listen(3000);
 console.log('Listening on port 3000...');
