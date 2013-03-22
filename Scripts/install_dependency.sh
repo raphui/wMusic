6c1bbe48adcf321a5ec4b66e8dd4038c7b658ccb
raphui <raphio134@hotmail.fr>
2013-03-22 04:41:19 -0700

    fix color

--- Scripts/install_dependency.sh
+++ Scripts/install_dependency.sh
@@ -5,18 +5,18 @@ wget https://developer.spotify.com/download/libspotify/libspotify-12.1.103-Linux
 tar -xvzf libspotify-12.1.103-Linux-armv6-bcm2708hardfp-release.tar.gz
 cd libspotify-12.1.103-Linux-armv6-bcm2708hardfp-release
 sudo make install prefix=/usr/local
-echo "\e[01;33mLibSpotify has been installed\e[00m"
+echo -e "\e[01;33mLibSpotify has been installed\e[00m"
 
 #Get vlc
 sudo apt-get install libvlc-dev
-echo "\e[01;33mLibVlc has been installed\e[00m"
+echo -e "\e[01;33mLibVlc has been installed\e[00m"
 
 #Get libasound2-dev
 sudo apt-get install libasound2-dev
-echo "\e[01;33mLibAsound2 has been installed\e[00m"
+echo -e "\e[01;33mLibAsound2 has been installed\e[00m"
 
 #Get node.js
 sudo apt-get install nodejs nodejs-dev
-echo "\e[01;33mNode.js has been installed\e[00m"
+echo -e "\e[01;33mNode.js has been installed\e[00m"
 
-echo "\e[01;33mAll dependency has been installed\e[00m"
+echo -e "\e[01;33mAll dependency has been installed\e[00m"
