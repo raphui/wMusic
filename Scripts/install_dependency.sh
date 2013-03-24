9b77503e0893ed70c154af7d85bf39837aeb71ea
raphui <raphio134@hotmail.fr>
2013-03-24 12:31:17 -0700

    fixing install_dep script

--- Scripts/install_dependency.sh
+++ Scripts/install_dependency.sh
@@ -4,7 +4,7 @@
 wget https://developer.spotify.com/download/libspotify/libspotify-12.1.103-Linux-armv6-bcm2708hardfp-release.tar.gz
 tar -xvzf libspotify-12.1.103-Linux-armv6-bcm2708hardfp-release.tar.gz
 cd libspotify-12.1.103-Linux-armv6-bcm2708hardfp-release
-sudo make install prefix=/usr/local
+sudo make install prefix=/usr/
 echo -e "\e[01;33mLibSpotify has been installed\e[00m"
 
 #Get vlc
